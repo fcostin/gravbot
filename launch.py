9ce24b2548784a54086afbab1a0e684321c898e5
Reuben Fletcher-Costin <reuben.fletchercostin@gmail.com>
2012-04-09 00:09:29 +1000

    improvements to launch.py
    made the logic inside main easier to follow
    also made --test and --profile play together

--- launch.py
+++ launch.py
@@ -2,28 +2,56 @@
 run the game, optionally with profiling ...
 """
 
+import sys
+
 def _run_app():
     from gravbot.app import App
     app = App()
     app.run()
-
 
 def _run_test_app():
     from gravbot.mc import App
     app = App()
     app.run()
 
+def _print_usage(out):
+    out.write('usage: [--profile] [--test]\n')
+    out.write('\t--profile : run with python profiling\n')
+    out.write('\t--test    : run in test mode (mc.py)\n')
+
 def _complain_about_usage_and_die():
-    sys.stderr.write('usage: [--profile]')
+    _print_usage(sys.stderr)
     sys.exit(1)
 
+def _wrap_with_profiling(entry_point):
+    """
+    run the given argumentless function entry_point using the python profiler
+
+    trap any SystemExits and KeyboardInterrupts so we can
+    still spam the profing results to stdout
+    """
+    def wrapped_entry_point():
+        import pstats
+        import cProfile
+        p = cProfile.Profile()
+        def safety_net(entry_point):
+            try:
+                entry_point()
+            except SystemExit:
+                pass
+            except KeyboardInterrupt:
+                pass
+        p.runcall(lambda : safety_net(entry_point))
+        s = pstats.Stats(p)
+        s.sort_stats('cumulative').print_stats(25)
+    return wrapped_entry_point
+
+
 def main():
-    import sys
     options = {
         '--profile': False,
-	'--test':False,
+        '--test': False,
     }
-
 
     for arg in sys.argv[1:]:
         if arg not in options:
@@ -31,22 +59,16 @@ def main():
         else:
             options[arg] = True
 
-    if options['--profile']:
-        import pstats
-        import cProfile
-        p = cProfile.Profile()
-        def _wrapper():
-            try:
-                _run_app()
-            except SystemExit, KeyboardInterrupt:
-                pass
-        p.runcall(_wrapper)
-        s = pstats.Stats(p)
-        s.sort_stats('cumulative').print_stats(25)
     if options['--test']:
-        _run_test_app()
+        entry_point = _run_test_app
     else:
-        _run_app()
+        entry_point = _run_app
+
+    if options['--profile']:
+        entry_point = _wrap_with_profiling(entry_point)
+
+    entry_point()
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
